eeac6bf3f4d003320a0c49db5c1c75335621d67f
Rimantas <belauzas@yahoo.com>
2019-09-23 20:42:33 +0300

    Achievements section rendering from objects

--- js/data.js
+++ js/data.js
@@ -1,6 +1,63 @@
-const achievements = [
+const achievements_demo = [
     ['archive', 245, 'Total projects'],
     ['trophy', 535, 'TROPHY WON'],
     ['users', 288, 'EXPERIENCES STAFF'],
     ['smile-o', 750, 'HAPPY CLIENTS']
 ];
+
+const achievements = [
+    {
+        icon: 'archive', 
+        number: 245, 
+        title: 'Total projects'
+    },
+    {
+        number: 535, 
+        icon: 'trophy', 
+        title: 'TROPHY WON'
+    },
+    {
+        icon: 'users', 
+        number: 288, 
+        title: 'EXPERIENCES STAFF'
+    },
+    {
+        icon: 'smile-o', 
+        number: 750, 
+        title: 'HAPPY CLIENTS'
+    },
+    {
+        icon: 'trophy', 
+        title: 'TROPHY WON'
+    },
+    {
+        icon: 'users', 
+        number: 288, 
+        title: 'EXPERIENCES STAFF'
+    },
+    {
+        icon: 'smile-o', 
+        number: 750, 
+        title: 'HAPPY CLIENTS'
+    }
+];
+
+
+// ['Vardenis', 'Pavardenis', 182, 105, 30]
+
+// const zmogus = {
+//     name: 'Vardenis',
+//     surname: 'Pavardenis',
+//     height: 182,
+//     weight: 105,
+//     age: 30,
+//     mother: {
+//         name: 'Vardene',
+//         surname: 'Pavardene',
+//         age: 60
+//     },
+//     marks: [1, 2, 3, 4, 5, 6]
+// }
+
+// const motinosVardas = zmogus.mother.name;
+// const treciasPazymys = zmogus.marks[2];
--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,34 @@
 function renderAchievements( data ) {
-    console.log(data);
     let HTML = '';
+    let quality = 0;
 
     for ( let i=0; i<data.length; i++ ) {
-        console.log(data[i]);
-        
         // HTML = HTML + template;
+        // HTML += '<div class="block">\
+        //             <i class="fa fa-'+ data[i][0] +'"></i>\
+        //             <p>'+ data[i][1] +'</p>\
+        //             <h4>'+ data[i][2] +'</h4>\
+        //         </div>';
+
+        // rejecting bad quality objects
+        if ( !data[i].icon ||
+             !data[i].number ||
+             !data[i].title ) {
+            continue;
+        }
+
+        // do not render more than 4 elements
+        if ( quality === 4 ) {
+            break;
+        }
+        
         HTML += '<div class="block">\
-                    <i class="fa fa-'+ data[i][0] +'"></i>\
-                    <p>'+ data[i][1] +'</p>\
-                    <h4>'+ data[i][2] +'</h4>\
+                    <i class="fa fa-'+ data[i].icon +'"></i>\
+                    <p>'+ data[i].number +'</p>\
+                    <h4>'+ data[i].title +'</h4>\
                 </div>';
+        quality++;
     }
-    
-    console.log(HTML);
     
     return document.getElementById('achievements').innerHTML = HTML;
 }
