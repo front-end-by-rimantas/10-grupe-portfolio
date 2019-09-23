b7a18e6d121978b4e18718101424c0735a40d592
Rimantas <belauzas@yahoo.com>
2019-09-23 19:39:36 +0300

    JS rendering achievements section

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 const achievements = [
-    ['box', 245, 'Total projects'],
-    ['box', 245, 'Total projects'],
-    ['box', 245, 'Total projects'],
-    ['box', 245, 'Total projects']
+    ['archive', 245, 'Total projects'],
+    ['trophy', 535, 'TROPHY WON'],
+    ['users', 288, 'EXPERIENCES STAFF'],
+    ['smile-o', 750, 'HAPPY CLIENTS']
 ];
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,19 @@
-function renderAchievements() {
-    return;
+function renderAchievements( data ) {
+    console.log(data);
+    let HTML = '';
+
+    for ( let i=0; i<data.length; i++ ) {
+        console.log(data[i]);
+        
+        // HTML = HTML + template;
+        HTML += '<div class="block">\
+                    <i class="fa fa-'+ data[i][0] +'"></i>\
+                    <p>'+ data[i][1] +'</p>\
+                    <h4>'+ data[i][2] +'</h4>\
+                </div>';
+    }
+    
+    console.log(HTML);
+    
+    return document.getElementById('achievements').innerHTML = HTML;
 }
